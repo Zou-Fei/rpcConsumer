fc30531433876e6eb1d53f954183afb6fd253cbe
zou-fei <Z1132205214>
2018-10-26 20:38:10 +0800

    fc

---
+++ src/main/java/com/example/rpcconsumer/RpcconsumerApplication.java
@@ -0,0 +1,12 @@
+package com.example.rpcconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcconsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcconsumerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/rpcconsumer/service/HelloService.java
@@ -0,0 +1,8 @@
+package com.example.rpcconsumer.service;
+
+public interface HelloService{
+
+    String hello(String name);
+
+    String hi(String msg);
+}
